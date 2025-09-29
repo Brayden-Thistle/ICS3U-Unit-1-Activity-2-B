d94e201efe2636c3759b98e5cd748521349dfeb6
default <bthistle@huronperthcatholic.ca>
2025-09-29 14:37:30 +0000

    made a code that greets me in TypeScript

--- main.ts
+++ main.ts
@@ -2,5 +2,10 @@
  * @author Brayden Thistle
  * @version 1.0.0
  * @date 2025-09-29
- * @fileoverview
+ * @fileoverview This program greets me when I run the program.
  */
+
+// print out the greeting
+console.log ("Hello, Brayden Thistle!");
+
+console.log("\nDone");
