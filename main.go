77c9ea348cbc2111f72116a923e9a8985e8e18c1
default <bthistle@huronperthcatholic.ca>
2025-09-29 14:45:47 +0000

    added a greeting to myself in .go file

---
+++ main.go
@@ -0,0 +1,17 @@
+/*
+ * Author: Brayden Thistle
+ * Version: 1.0.0
+ * Date: 2025-09-29
+ * This program greets me with my name.
+ */
+
+ package main
+
+ import "fmt"
+
+ func main() {
+	//print out the greeting
+	fmt.Println("Hello, Brayden Thistle!")
+	
+	fmt.Println("\nDone.")
+ }
